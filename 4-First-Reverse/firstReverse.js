dbfb8ff2550d80dd15d91d06d9d0d05acbfc1fa4
Merovingian <52575351+furkananter@users.noreply.github.com>
2022-08-25 23:08:19 +0300

    First 7 Questions added!

---
+++ 4-First-Reverse/firstReverse.js
@@ -0,0 +1,32 @@
+export const firstReverse = () => {
+    // code will be here.
+};
+
+/*
+    ENG:
+TODO: Rules:
+    *1. 
+    *2. 
+    *3. 
+    *4.  
+     
+     ? Something
+
+    TR:
+TODO:  Kurallar:
+    *1. 
+    *2. 
+    *3. 
+    *4. 
+
+    ? Something
+
+    DE: 
+TODO: Rules:
+    *1. 
+    *2. 
+    *3. 
+    *4. 
+
+    ? Something
+*/
---
+++ 5-Find-Intersection/findIntersection.js
@@ -0,0 +1,32 @@
+export const findIntersection = () => {
+    // code will be here.
+};
+
+/*
+    ENG:
+TODO: Rules:
+    *1. 
+    *2. 
+    *3. 
+    *4.  
+     
+     ? Something
+
+    TR:
+TODO:  Kurallar:
+    *1. 
+    *2. 
+    *3. 
+    *4. 
+
+    ? Something
+
+    DE: 
+TODO: Rules:
+    *1. 
+    *2. 
+    *3. 
+    *4. 
+
+    ? Something
+*/
