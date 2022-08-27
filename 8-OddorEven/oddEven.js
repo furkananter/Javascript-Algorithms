d01434e547e50bfadf4e4d281d3341ce8b8eaa84
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-08-28 00:36:51 +0300

    I think quite good question

---
+++ 8-OddorEven/oddEven.js
@@ -0,0 +1,54 @@
+export const oddEven = (num) => {
+    let number = num
+    number.toString().split("").map(iNumb => parseInt(iNumb,10)).reduce((a,b) => a + b);
+    return number % 2 === 0 ? "Even" : "Odd";
+};
+
+// Yönerge:
+/*
+    Bir sayının Tek mi Çift mi olduğunu belirleyen bir işlev oluşturun. 
+    Bir sayı, tüm basamaklarının toplamı tek ise tek, tüm basamaklarının toplamı çift ise bir sayıdır. 
+    Bir sayı tek ise, "Odd" döndürün. Aksi takdirde, "Even" değerini döndürün. Örnek olarak:
+
+oddishOrEvenish(43) ➞ "Odd"
+    4 + 3 = 7
+    7 % 2 = 1
+
+oddishOrEvenish(373) ➞ "Odd"
+    3 + 7 + 3 = 13
+    13 % 2 = 1
+
+oddishOrEvenish(4433) ➞ "Even"
+    4 + 4 + 3 + 3 = 14
+    14 % 2 = 0
+
+*/
+
+/*
+    ENG:
+TODO: Rules:
+    *1. I'll explain everything soon.
+    *2. 
+    *3. 
+    *4.  
+     
+     ? Something
+
+    TR:
+TODO:  Kurallar:
+    *1. Detaylı Açıklayacağım.
+    *2. 
+    *3. 
+    *4. 
+
+    ? Something
+
+    DE: 
+TODO: Rules:
+    *1.  Warte, bitte.
+    *2. 
+    *3. 
+    *4. 
+
+    ? Something
+*/
--- easy.js
+++ easy.js
@@ -1,5 +1,15 @@
-// 1. import { validateUser } from "./1-User-Validation/userValidation.js";
-import { longestWord } from "./2-Longest-Word/longestWord.js";
+// 1. 
+// import { validateUser } from "./1-User-Validation/userValidation.js";
+
+//2.
+//import { longestWord } from "./2-Longest-Word/longestWord.js";
+
+//3.
+//import { firstFactorial } from "./3-First-Factorial/firstFactorial.js";
+
+// 8. 
+import { oddEven } from "./8-OddorEven/oddEven.js";
+console.log(oddEven(213));
 
 // 1. console.log(validateUser("u___username_123"));
 
